ddbe7099b609cb9294cb7c39d8702ace7ec0809a
Jose Velez <empire@Joses-MBP-2.lan>
2017-04-01 17:57:39 -0400

    First upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'P2M5UAYrAPiD2ZRiPm8QBmzfM',
+  consumer_secret: 'TlyTNtImq3Yh8nqN21QAZG1ij8t8ppVqzBaoLPD15EKtJ9JQK2',
+  access_token_key: '265727451-mFcbfnHIS9bGLaIoqZ8gBtVp8S9aDEeVShxnEWgl',
+  access_token_secret: 'LyvZLfME2NnKi4dCltj4QsW6u0RFcRUh8Alk8qnHPDYcV',
+}
