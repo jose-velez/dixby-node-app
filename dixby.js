c45d2b4cfa8314aee299908fb909382f33cd4b9a
Jose Velez <empire@Joses-MacBook-Pro-2.local>
2017-04-05 18:14:21 -0400

    Read file working

--- dixby.js
+++ dixby.js
@@ -2,10 +2,13 @@ console.log("it is working");
 var keys= require("./keys.js");
 var Twitter = require('twitter');
 var spotify = require('spotify');
+var request = require("request");
+var fs = require("fs");
 console.log(keys.twitterKeys.consumer_key);
 //Variable to store the song
 var search = process.argv[2];
 var song = [];
+var movie = [];
 switch (search) {
   case "spotify-this-song":
     song = process.argv[3];
@@ -18,41 +21,37 @@ switch (search) {
   case "my-tweets":
     twitter();
     break;
+  case "movie-this":
+  movie = process.argv[3];
+  movieSearch(movie);
+  break;
+  case "do-what-it-says":
+    readFile();
+    break;
   default:
 
 }
 
 
-
+// function to call spotify
 function searchSpotify(song){
   spotify.search({ type: 'track', query: song }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
-
+    console.log("_______________________________________________________");
     var artist = data.tracks.items[0].album.artists[0].name;
-    console.log(artist);
+    console.log("Artist: " + artist);
     var songName = data.tracks.items[0].name;
-    console.log(songName);
+    console.log("Song Name: " + songName);
     var songLink = data.tracks.items[0].external_urls.spotify;
-    console.log(songLink);
+    console.log("Link: " + songLink);
     var album = data.tracks.items[0].album.name;
-    console.log(album);
+    console.log("Album: " + album);
+    console.log("_______________________________________________________");
 });
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 // Function to call twitter
 function twitter(){
@@ -77,3 +76,23 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 
 };
+
+//Function to call OMDB
+function movieSearch(movie)
+{
+  request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurre
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log(body.title);
+  console.log(response.title);
+});
+};
+//Function to read the file
+function readFile(){
+  fs.readFile("random.txt", "utf8", function(error, data){
+    var dataArr = data.split(",");
+    console.log(dataArr);
+    searchSpotify(dataArr[1])
+  })
+}
